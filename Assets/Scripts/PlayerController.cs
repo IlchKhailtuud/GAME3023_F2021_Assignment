625215921cd805522ba98478a5e50411954d5773
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-09-21 00:03:57 -0400

    Imported Art Assets Implemented Character Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    private float moveSpeed;
+    private bool isMoving;
+    private Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            if (!isMoving)
+            {
+                input.x = Input.GetAxisRaw("Horizontal");
+                input.y = Input.GetAxisRaw("Vertical");
+                
+                //remove diagonal movement 
+                if (input.x != 0)
+                    input.y = 0;
+
+                if (input != Vector2.zero)
+                {
+                    Vector2 targetPos = transform.position;
+                    targetPos.x += input.x;
+                    targetPos.y += input.y;
+
+                    StartCoroutine(Move(targetPos));
+                }
+            }
+        }
+        
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        } 
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
