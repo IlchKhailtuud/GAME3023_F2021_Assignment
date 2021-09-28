100b935ffe9eae5f208285ccc603431b5ce96351
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-09-27 21:02:19 -0400

    Added lights using URP Player colliding with other game objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private bool isMoving;
+    private float inputX;
+    private float inputY;
+    
     [SerializeField] 
     private float moveSpeed;
-    private bool isMoving;
-    private Vector2 input;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,34 +22,26 @@ public class PlayerController : MonoBehaviour
     {
         if (!isMoving)
         {
-            if (!isMoving)
+            inputX = Input.GetAxisRaw("Horizontal");
+            inputY = Input.GetAxisRaw("Vertical");
+            
+            if (inputX != 0 || inputY != 0)
             {
-                input.x = Input.GetAxisRaw("Horizontal");
-                input.y = Input.GetAxisRaw("Vertical");
+                Vector2 targetPos = transform.position;
+                targetPos.x += inputX;
+                targetPos.y += inputY;
                 
-                //remove diagonal movement 
-                if (input.x != 0)
-                    input.y = 0;
-
-                if (input != Vector2.zero)
-                {
-                    Vector2 targetPos = transform.position;
-                    targetPos.x += input.x;
-                    targetPos.y += input.y;
-
-                    StartCoroutine(Move(targetPos));
-                }
+                StartCoroutine(Move(targetPos));
             }
         }
-        
     }
 
-    IEnumerator Move(Vector3 targetPos)
+    IEnumerator Move(Vector2 targetPos)
     {
         isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        while ((targetPos - (Vector2)transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         } 
         transform.position = targetPos;
